907d1da5e299c946f1603692d257353fccdaeb87
willorme <wjorme98@gmail.com>
2022-01-29 13:30:54 -0600

    added deposit function

--- atm.py
+++ atm.py
@@ -4,29 +4,51 @@ class Customer:
         self.age = age
         self.balance = balance
 
-customers = [Customer('Will', '23', str(62500)),
-Customer('Amanda', '22', str(10000000))]
+
+
+customers = [Customer('Will Orme', '23', str(62500)),
+Customer('Amanda Keller', '22', str(10000000))]
+
+
 
 def withdraw():
     for customer in customers:
         q1 = input('Enter your name: ')
-        if q1.__contains__(customer.name):
+        if q1 == customer.name:
             print (customer.name + ', your current balance is ' + customer.balance)
         else:
             print ('Name does not match any account')
             break
         q2 = input ('Enter amount to withdraw: ')
         new_balance = int(customer.balance) - int(q2)
-        if (q2) > customer.balance:
+        if q2 > customer.balance:
             print ('Insufficient funds')
             break
         else: 
             print('Your balance is now ' + str(new_balance))
             break
 
+def deposit():
+    for customer in customers:
+        q1 = input('Enter your name: ')
+        if q1 == customer.name:
+            print (customer.name + ', your current balance is ' + customer.balance)
+        else:
+            print ('Name does not match any account')
+            break
+        q2 = input ('Enter amount to deposit: ')
+        new_balance = int(customer.balance) + int(q2)
+        if int(q2) <= 0:
+            print('Must deposit amount greater than 0')
+        else:
+            print('Your balance is now: ' + str(new_balance))
+            break
+
             
-            
-withdraw() 
+def menu():
+    pass 
+
+
             
         
 
