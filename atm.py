d84f59d72ba3151eba9aea5e93cb8caae674260a
willorme <wjorme98@gmail.com>
2022-01-31 17:35:38 -0600

    Added menu funciton

--- atm.py
+++ atm.py
@@ -1,21 +1,23 @@
+from locale import currency
+import locale
+
+locale.setlocale(locale.LC_ALL, '')
+
 class Customer:
     def __init__(self, name, age, balance):
         self.name = name
         self.age = age
         self.balance = balance
 
-
-
-customers = [Customer('Will Orme', '23', str(62500)),
+customers = [Customer('Will Orme', '23', str(62500)), 
 Customer('Amanda Keller', '22', str(10000000))]
-
 
 
 def withdraw():
     for customer in customers:
         q1 = input('Enter your name: ')
         if q1 == customer.name:
-            print (customer.name + ', your current balance is ' + customer.balance)
+            print (customer.name + ', your current balance is ' + currency(int(customer.balance)))
         else:
             print ('Name does not match any account')
             break
@@ -25,14 +27,14 @@ def withdraw():
             print ('Insufficient funds')
             break
         else: 
-            print('Your balance is now ' + str(new_balance))
+            print('Your balance is now ' + currency(int(new_balance)))
             break
 
 def deposit():
     for customer in customers:
         q1 = input('Enter your name: ')
         if q1 == customer.name:
-            print (customer.name + ', your current balance is ' + customer.balance)
+            print (customer.name + ', your current balance is ' + currency(int(customer.balance)))
         else:
             print ('Name does not match any account')
             break
@@ -41,14 +43,23 @@ def deposit():
         if int(q2) <= 0:
             print('Must deposit amount greater than 0')
         else:
-            print('Your balance is now: ' + str(new_balance))
+            print('Your balance is now: ' + currency(int(new_balance)))
             break
 
             
 def menu():
-    pass 
+    print('################ Welcome to Orme Bank ##################')
+    main = input('Would you like to Deposit or Withdraw? ')
+    if main == 'Withdraw':
+        withdraw()
+    elif main == 'Deposit':
+        deposit()
+    else:
+        print ('Invalid input')
 
 
+
+menu()
             
         
 
