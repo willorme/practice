f41e3b0cb48e35cb48e99f55fbcd5e607efbb7a6
willorme <wjorme98@gmail.com>
2022-02-20 17:52:00 -0600

    reorganized code

--- python/atm.py
+++ python/atm.py
@@ -12,59 +12,66 @@ class Customer:
 
 
 
-customers = [Customer('Amanda Keller', 22, 10000000.00),Customer('Will Orme', 23, 10000000.00)
+customers = [Customer('Amanda Keller', 22, 10000000.00),Customer('Will Orme', 23, 20000000.00)
 ,Customer('Steve Orme', 26, 25.00)]
     
 
-
-def withdraw():
-    for customer in customers:
-
-        q = input ('Enter amount to withdraw: ')
-        new_balance = customer.balance - float(q)
-        print('Withdrawing cash...')
-        time.sleep(3)
-        if float(q) > customer.balance:
-            print ('Insufficient funds')
-            continue
-        else: 
-            print('Your balance is now {}'.format(currency(float(new_balance))))
-            break
-
-
-def deposit():
-    for customer in customers:
-    
-        q = input ('Enter amount to deposit: ')
-        new_balance = customer.balance + float(q)
-        print('Adding funds to your account...')
-        time.sleep(3)
-        if float(q) <= 0:
-            print('Must deposit amount greater than 0')
-            continue
-        else:
-            print('Your balance is now {}'.format(currency(float(new_balance))))
-            break
 
 
 def menu():
     print('################ Welcome to Orme Bank ##################')
    
-    for customer in customers:
+    for customer in range(len(customers)):
+        
+        balance = customers[customer].balance
+        name = customers[customer].name
 
         main = input('Enter your name: ')
-        if main == customer.name:
-            print ('{}, your current balance is {}'.format(customer.name, currency(float(customer.balance))))
+        if main == name:
+            print ('{}, your current balance is {}'.format(name, currency(float(balance))))
         else:
             print ('Name does not match existing account')
             continue
         wd = input('Would you like to withdraw or deposit? ')
+
+
+
+        
         if wd == 'withdraw':
-            withdraw()
-            break
+
+    
+
+            q = input ('Enter amount to withdraw: ')
+            new_balance = balance - float(q)
+            print('Withdrawing cash...')
+            time.sleep(3)
+            if float(q) > balance:
+                print ('Insufficient funds')
+                continue
+            else: 
+                print('Your balance is now {}'.format(currency(float(new_balance))))
+                break
+
+    
+            
         elif wd == 'deposit':
-            deposit()
-            break
+            
+           
+
+            q = input ('Enter amount to deposit: ')
+            new_balance = balance + float(q)
+            print('Adding funds to your account...')
+            time.sleep(3)
+            if float(q) <= 0:
+                print('Must deposit amount greater than 0')
+                continue
+            else:
+                print('Your balance is now {}'.format(currency(float(new_balance))))
+                break
+            
+
+            
+
         else:
             print ('Invalid input')
         continue
